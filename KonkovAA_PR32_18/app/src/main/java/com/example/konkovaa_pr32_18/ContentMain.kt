b91cb21179c21bcd64aa017e29ac86f67ba621ab
Ghosterenk0 <arsenijkonkov912@gmail.com>
2025-12-17 11:31:37 +0500

    Update ContentMain.kt

--- KonkovAA_PR32_18/app/src/main/java/com/example/konkovaa_pr32_18/ContentMain.kt
+++ KonkovAA_PR32_18/app/src/main/java/com/example/konkovaa_pr32_18/ContentMain.kt
@@ -7,41 +7,25 @@ import android.view.View
 import android.widget.Button
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.konkovaa_pr32_18.databinding.ContentMainBinding
 
 class ContentMain : AppCompatActivity() {
 
-    lateinit var appBarConfiguration: AppBarConfiguration
-    lateinit var binding: ContentMainBinding
-
+    val key = "COUNTKEY";
+    var count = 0;
     override fun onCreate(savedInstanceState: Bundle?) {
+        var binding: ContentMainBinding
         super.onCreate(savedInstanceState)
-
-        val key = "COUNTKEY";
-        val setting = getSharedPreferences("PFNAME", MODE_PRIVATE)
-
-        fun getCount(): Int {
-            return setting.getInt(key, 0)
-        }
-        fun setCount(count: Int) {
-            val pref = setting.edit()
-            pref.putInt(key, count).apply()
-        }
-
 
         binding = ContentMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-
+        val setting: SharedPreferences = getSharedPreferences("PFNAME", MODE_PRIVATE)
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Вы нажали на FloatingActionBar", Snackbar.LENGTH_LONG).show()
         }
-
+        count = setting.getInt(key, 0)
 
         val btn1 = findViewById<Button>(R.id.btn1)
         val btn2 = findViewById<Button>(R.id.btn2)
@@ -53,15 +37,16 @@ class ContentMain : AppCompatActivity() {
         btn2.setOnClickListener {
             val snakebar = Snackbar.make(it, "Вы изминили что-то", Snackbar.LENGTH_LONG)
             snakebar.setActionTextColor(Color.RED)
-            snakebar.setAction("ВЕРНУТЬ КАК БЫЛО?", View.OnClickListener { Snackbar.make(btn2, "Все вернулось на свои места", Snackbar.LENGTH_LONG) })
+            snakebar.setAction("ВЕРНУТЬ КАК БЫЛО?", View.OnClickListener { Snackbar.make(btn2, "Все вернулось на свои места", Snackbar.LENGTH_LONG).show() })
             snakebar.show()
         }
         btn3.setOnClickListener {
-            val count = 0;
-            val snakebar = Snackbar.make(it, "Повторите еще раз! ${getCount()}" , Snackbar.LENGTH_LONG)
+            val snakebar = Snackbar.make(it, "Повторите еще раз! ${count}" , Snackbar.LENGTH_LONG)
             snakebar.setActionTextColor(Color.CYAN)
             snakebar.setAction("ПОВТОРИТЬ", View.OnClickListener {
-                setCount(count + 1);
+                count++
+                setting.edit().putInt(key, count).apply()
+
             })
             snakebar.show()
         }
